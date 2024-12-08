22e6fce12adeea9cb462cc28d2d9d5efa6feb759
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2024-12-08 12:46:25 +0100

    Day 8, part 2

--- src/day08/part2.rs
+++ src/day08/part2.rs
@@ -1,3 +1,65 @@
+use super::{Map, Point};
+use std::collections::HashSet;
+
 pub fn solve() -> String {
-    "".to_string()
+    let map = super::get_input();
+    let mut antinodes: HashSet<Point> = HashSet::new();
+
+    for antennas in map.antenna.values() {
+        for antenna1 in antennas {
+            for antenna2 in antennas {
+                if antenna1 == antenna2 {
+                    continue;
+                }
+
+                let point_diff = normalized_diff(*antenna1, *antenna2);
+                let mut current_point = *antenna1;
+
+                while let Some(antinode) = map.verify_point((
+                    current_point.0 as i32 + point_diff.0,
+                    current_point.1 as i32 + point_diff.1,
+                )) {
+                    antinodes.insert(antinode);
+                    current_point = antinode;
+                }
+            }
+        }
+    }
+
+    print_map(&map, &antinodes);
+    antinodes.len().to_string()
+}
+
+fn normalized_diff(a: Point, b: Point) -> (i32, i32) {
+    let dist = (b.0 as i32 - a.0 as i32, b.1 as i32 - a.1 as i32);
+    let gcd = gcd(dist.0.abs(), dist.1.abs());
+    (dist.0 / gcd, dist.1 / gcd)
+}
+
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn print_map(map: &Map, antinodes: &HashSet<Point>) {
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let point = (x as u8, y as u8);
+            if let Some(freq) = map
+                .antenna
+                .iter()
+                .find(|(_freq, antennas)| antennas.contains(&point))
+            {
+                print!("{}", freq.0);
+            } else if antinodes.contains(&point) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -45,5 +45,5 @@ fn print_day(day_num: u32, part: u32) {
 }
 
 fn main() {
-    print_day(8, 1);
+    print_day(8, 2);
 }
